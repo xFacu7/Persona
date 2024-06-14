235443e9ea06842ad8ae76a03f1e77eaf39622aa
xFacu7 <129421571+xFacu7@users.noreply.github.com>
2024-06-13 22:26:42 -0300

    Add files via upload

---
+++ Persona/Mpersona.cs
@@ -0,0 +1,32 @@
+﻿using Principal;
+
+namespace LibreriaPersona{
+
+    public class Persona{
+        
+        
+        public string Nombre;
+        public int Dni;
+        public string Fec_Nac;
+        public bool vivo;
+        
+        public Persona(int unDni,string unNombre,string unFec_Nac,bool unvivo){
+            Dni = unDni;
+            Nombre = unNombre;
+            Fec_Nac = unFec_Nac;
+            vivo = unvivo;
+        }
+        public string Respirar(){
+            return "Respirando...";
+        }
+        public void morir(){
+            Console.WriteLine("Persona murio...");
+            vivo = false;
+        }
+        public void revivir()
+        {
+            Console.WriteLine("Persona revivio...");
+            vivo = true;
+        }
+    }
+}
---
+++ Persona/Program.cs
@@ -0,0 +1,22 @@
+﻿using LibreriaPersona;
+
+namespace Principal{
+
+    public class Programa{
+
+
+        static void Main(){
+
+        Persona p1 = new Persona( 4570,"Pedro", "19/03/2000",true);
+
+        Console.WriteLine(p1.Dni);
+        Console.WriteLine(p1.Nombre);
+        Console.WriteLine(p1.Fec_Nac);
+        Console.WriteLine(p1.vivo);
+        Console.WriteLine(p1.Respirar());
+        p1.morir();
+        p1.revivir();
+    }
+
+    }
+}
