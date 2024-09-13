ad70a6bb0a253fafa0e195d55ed04d2b30c97fc6
xFacu7 <129421571+xFacu7@users.noreply.github.com>
2024-09-12 22:06:45 -0300

    Add files via upload

---
+++ Labo TN Persona/objetoPersona/Clases.cs
@@ -0,0 +1,110 @@
+using MySql.Data.MySqlClient;
+using System.Data;
+
+namespace libreriaClase{
+    class Persona
+    {
+
+//        private string nombre;
+
+
+        public string Apellido {get;set;}
+        public string Nombre {get;set;}
+        public int DNI {get;set;}
+        public string FechaNacimiento {get;set;}
+
+
+       
+
+        public  Persona(string rApellido,string rNombre, string rFechaNacimiento, int rDni){
+            Apellido = rApellido;
+            Nombre = rNombre;
+            FechaNacimiento = rFechaNacimiento;
+            DNI = rDni;
+
+        }
+
+        public Persona(){
+            
+        }
+        public string caminar(){
+
+            return "Caminando....";
+
+        }
+
+        public void mostrarPersona(){
+            Console.WriteLine("Apellido: {0}", Apellido);
+            Console.WriteLine("Nombre: {0}", Nombre);
+            Console.WriteLine("Fecha Nacimiento: {0}", FechaNacimiento);
+            Console.WriteLine("DNI: {0}", DNI);
+        }
+
+        public void devolverEdad(){
+            Console.WriteLine("La edad es:.....");
+        }
+
+    }
+
+    class Alumno : Persona {
+
+        private int legajo;
+        private string curso;
+
+        private int[] notas;
+
+
+        public void estudiar(){
+            Console.WriteLine("Estudiando....");
+        }
+
+        public void devolverCurso(){
+            Console.WriteLine("El curso es: {0}", this.curso);
+        }
+    }
+
+    class conexionBD{
+
+        MySqlConnection Conector; 
+        MySqlCommand Comando;
+
+        public void conectar(){
+
+            Conector = new MySqlConnection(@"server=127.0.0.1; database=5to_Persona; Uid=5to_agbd; pwd=Trigg3rs!");
+            Comando = Conector.CreateCommand();
+
+        }
+
+        public void insertarBD(Persona rPersona){
+
+                Comando.CommandText = "insert into Persona (DNI,Apellido,Nombre,FechaNacimiento)  values ('" + rPersona.DNI + "', '" + rPersona.Apellido + "','" + rPersona.Nombre + "','" + rPersona.FechaNacimiento + "')";
+
+                Comando.CommandType = CommandType.Text;
+                Conector.Open();
+                Comando.ExecuteNonQuery();    
+                Conector.Close();        
+
+        }
+
+        public object countPersonNameBD(string rnombre){
+
+                object cantidadObjeto;
+                
+                Comando.CommandText = "select count(*) from Persona where Nombre = '" + rnombre +"'";
+                
+                Comando.CommandType = CommandType.Text;
+                Conector.Open();
+                cantidadObjeto = Comando.ExecuteScalar();   
+                
+                Conector.Close();        
+
+                return cantidadObjeto;
+
+        }
+
+        public object MostrarBD()
+        {
+            
+        }
+    }
+}
---
+++ Labo TN Persona/objetoPersona/Principal.cs
@@ -0,0 +1,218 @@
+﻿using System;
+using System.Security.AccessControl;
+using System.Collections.Generic;  //librería de LIST
+using libreriaClase;
+using System.Security.Cryptography.X509Certificates;
+
+namespace libreria
+{
+
+    class Pricipal{
+
+        
+
+        static void Main(){
+
+            List<Persona> listaPersona = new List<Persona>();
+
+            string opcion = mostrarMenu();
+
+            while ( opcion != "9") {
+
+
+                switch(opcion) 
+                            {
+                            case "1":
+                                listaPersona.Add(crearPersona());
+                                break;
+                            case "2":
+                                mostrarPersona(listaPersona);
+                                break;
+                            case "3":
+                                buscarPersona(listaPersona);
+                                break;
+
+                            case "4":
+                                insertarPersona(listaPersona);
+                                break;
+                            case "5":
+                                contarNombrePersonas();
+                                break;
+                            case "6":
+                                MostrarAlumnoBD();
+                                break;
+
+                            default:
+                                Console.WriteLine("Opción Incorrecta...");
+                                Console.ReadKey();
+                                break;
+                            }
+
+            opcion = mostrarMenu();
+
+            }
+
+        }
+
+         public static Persona crearPersona(){
+
+            Console.Clear();
+            Persona p1 = new Persona();
+
+            Console.WriteLine("Ingreso de Personas");
+            Console.WriteLine("-------------------");
+            Console.WriteLine("");
+
+            Console.Write("Ingrese Apellido: ");
+            p1.Apellido = Console.ReadLine();
+
+            Console.Write("Ingrese Nombre: ");
+            p1.Nombre = Console.ReadLine();
+
+            Console.Write("Ingrese DNI: ");
+            p1.DNI = Convert.ToInt32(Console.ReadLine());            
+            
+
+            Console.Write("Ingrese Fecha Nacimiento: ");
+            p1.FechaNacimiento = Console.ReadLine();
+
+            return p1;
+
+        }
+
+        public static void mostrarPersona(List<Persona> personas){
+                Console.Clear();
+                foreach(var elem in personas){
+                    
+                    Console.Write("Apellido: ");
+                    Console.WriteLine(elem.Apellido);
+                    Console.Write("Nombre: ");
+                    Console.WriteLine(elem.Nombre);
+                    Console.Write("DNI:");
+                    Console.WriteLine(elem.DNI);
+                    Console.Write("Fecha Nacimiento:");
+                    Console.WriteLine(elem.FechaNacimiento);
+                    Console.WriteLine("----------------");
+
+                    
+                }
+                Console.ReadKey();
+        }
+
+    public static void buscarPersona(List<Persona> personas){
+                int dni;
+                int flag=0;
+                Console.Clear();
+                Console.Write("Ingrese DNI Persona a buscar: ");
+                dni = Convert.ToInt32(Console.ReadLine());
+
+                foreach(var elem in personas){
+
+                    if ( elem.DNI == dni ){
+
+                        Console.Write("Apellido: ");
+                        Console.WriteLine(elem.Apellido);
+                        Console.Write("Nombre: ");
+                        Console.WriteLine(elem.Nombre);
+                        Console.Write("DNI:");
+                        Console.WriteLine(elem.DNI);
+                        Console.Write("Fecha Nacimiento:");
+                        Console.WriteLine(elem.FechaNacimiento);
+                        Console.WriteLine("----------------");
+
+                        
+
+                        Console.ReadKey();
+                        flag = 1;
+
+                    }
+
+                }
+                if (flag == 0){
+                    Console.WriteLine("Registro No Encontrado");
+                    Console.ReadKey();
+                }
+                
+
+
+
+
+    }
+
+        public static void insertarPersona(List<Persona> rlistaPersona){
+
+            conexionBD con  = new conexionBD();
+
+            con.conectar();
+
+            foreach(var p in rlistaPersona){
+
+                con.insertarBD(p);
+            }
+
+            Console.WriteLine("Registros Insertados....");
+            Console.WriteLine("Presione una tecla para continuar....");
+            Console.ReadKey();
+
+        }
+        public static void contarNombrePersonas(){
+
+            conexionBD con  = new conexionBD();
+
+            con.conectar();
+        
+
+            string nombre;
+
+            Console.Clear();
+            Console.Write("Ingrese Nombre: ");
+            nombre = Console.ReadLine();
+
+            Console.WriteLine("");
+            Console.WriteLine("El nombre ingresado existe {0} Veces.", con.countPersonNameBD(nombre));
+            Console.WriteLine("Presione una tecla para continuar....");
+            Console.ReadKey();        
+
+        }
+
+        public static void MostrarAlumnoBD()
+        {
+
+             conexionBD con  = new conexionBD();
+
+            con.conectar();
+
+        }
+        public static string mostrarMenu(){
+
+            string opcion;
+
+            Console.Clear();
+
+            Console.WriteLine("Menú");
+            Console.WriteLine("-----");
+
+            Console.WriteLine("");
+            Console.WriteLine("1.- Crear Alumno");
+            Console.WriteLine("2.- Mostrar Alumnos");
+            Console.WriteLine("3.- Buscar Alumno");
+            Console.WriteLine("4.- Exportar Alumnos a BD");
+            Console.WriteLine("5.- Contar la cantidad de....");
+            Console.WriteLine("6.- Mostrar Alumno BD");
+
+            
+
+            Console.WriteLine("");
+            Console.Write("Elija una opción: ");
+
+            opcion = Console.ReadLine();
+
+            return opcion;
+
+
+        }
+    }
+
+
+    
+}
